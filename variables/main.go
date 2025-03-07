911de06811eabbf8e6c2dcc4d01a260f97fbd556
Fellipe Juncal <fjuncal7@gmail.com>
2025-03-07 20:03:47 -0300

    Creating class main.go inside variables

---
+++ variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Philippe"
+	age := 28 // Inferência de tipo
+
+	const PI = 3.1415 // Constante
+
+	fmt.Println("Nome:", name)
+	fmt.Println("Idade:", age)
+	fmt.Println("PI:", PI)
+}
